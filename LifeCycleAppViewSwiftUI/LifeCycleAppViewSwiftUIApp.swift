d4224ae80b9d92054a1ea1fc4da95d067114e6af
Luis Angel Inga Mendoza <lingamen@emeal.nttdata.com>
2023-02-06 11:54:29 -0500

    Initial Commit

---
+++ LifeCycleAppViewSwiftUI/LifeCycleAppViewSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifeCycleAppViewSwiftUIApp.swift
+//  LifeCycleAppViewSwiftUI
+//
+//  Created by Luis Angel Inga Mendoza on 6/02/23.
+//
+
+import SwiftUI
+
+@main
+struct LifeCycleAppViewSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
